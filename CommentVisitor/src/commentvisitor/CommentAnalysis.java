3617ffda79bf078f110cd58a8a149b1a0550f749
Joao F. Ferreira <joao@joaoff.com>
2014-03-24 10:40:23 +0000

    Updated the comment analysis to extract line and block comments (together with their parent ASTNode).

--- CommentVisitor/src/commentvisitor/CommentAnalysis.java
+++ CommentVisitor/src/commentvisitor/CommentAnalysis.java
@@ -8,10 +8,13 @@ import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.BlockComment;
+import org.eclipse.jdt.core.dom.Comment;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.LineComment;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.NodeFinder;
 
 import testanalysis.TestAnalysis;
 import edu.cmu.cs.crystal.AbstractCrystalMethodAnalysis;
@@ -24,29 +27,47 @@ public class CommentAnalysis extends AbstractCrystalMethodAnalysis {
 	long startTime;
 	long endTime;
 
+	CommentVisitor cvisitor; // This is the comment visitor used to visit all
+								// the AST Nodes; it is initialised in
+								// beforeAllMethods()
+
 	// Called before all methods
 	@Override
 	public void beforeAllMethods(ITypeRoot compUnit, CompilationUnit rootNode) {
 		List<ASTNode> commentList1 = rootNode.getCommentList();
-		
-		for(int i=0;i<commentList1.size();i++){
-			//print each comment in the list
-		System.out.println(commentList1.get(i));
+
+		String source = null;
 		try {
-			//print the name of the method associated with the comment
-			System.out.println(compUnit.getElementAt(commentList1.get(i).getStartPosition()).getJavaModel());
+			source = compUnit.getSource();
 		} catch (JavaModelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// TODO: what is the best way of terminating the analysis?
+			System.exit(-1);
 		}
+
+		if (source == null) {
+			// TODO: what is the best way of terminating the analysis?
+			System.exit(-1);
 		}
-		
-		
-		
-		
-		// Initialises this lists
+
+		// Property: At this point, source exists and is not null.
+
+		// initialize the cvisitor with the source
+		cvisitor = new CommentVisitor(rootNode, source.split("\n"));
+
+		// To visite LineComment and BlockComment nodes, we need to call the
+		// accept method on them
+		// (see documentation of getCommentList() for more details)
+		List<Comment> l = rootNode.getCommentList();
+		for (Comment c : l) {
+			if (!(c instanceof Javadoc)) {
+				c.accept(cvisitor);
+			}
+		}
+
+		// Initialises these lists
 		javadocList = new ArrayList<JavadocContext>();
 		commentList = new ArrayList<CommentContext>();
+
 		// records start time of analysis
 		startTime = System.currentTimeMillis();
 		System.out.println("Running: " + this.getName());
@@ -56,7 +77,7 @@ public class CommentAnalysis extends AbstractCrystalMethodAnalysis {
 	// Called after all methods
 	@Override
 	public void afterAllMethods(ITypeRoot compUnit, CompilationUnit rootNode) {
-		
+
 		// records end time
 		endTime = System.currentTimeMillis();
 		// prints time taken
@@ -70,7 +91,7 @@ public class CommentAnalysis extends AbstractCrystalMethodAnalysis {
 
 	@Override
 	public void analyzeMethod(MethodDeclaration d) {
-		d.accept(new CommentVisitor());
+		d.accept(cvisitor);
 	}
 
 	@Override
@@ -80,6 +101,15 @@ public class CommentAnalysis extends AbstractCrystalMethodAnalysis {
 
 	// visitor to find comments
 	private class CommentVisitor extends ASTVisitor {
+
+		CompilationUnit compUnit;
+		String[] source;
+
+		public CommentVisitor(CompilationUnit c, String[] s) {
+			compUnit = c;
+			source = s;
+		}
+
 		// called whenever javadoc comment is found
 		@Override
 		public void endVisit(Javadoc node) {
@@ -89,12 +119,62 @@ public class CommentAnalysis extends AbstractCrystalMethodAnalysis {
 
 		@Override
 		public boolean visit(LineComment node) {
-			System.out.println("LINE COMMENT");
+
+			System.out.println("BEGIN LINE COMMENT");
+			int startLineNumber = compUnit.getLineNumber(node
+					.getStartPosition()) - 1;
+			String lineComment = source[startLineNumber].trim();
+
+			System.out.println("@@@@@");
+			System.out.println(lineComment);
+
+			int start = node.getStartPosition();
+			ASTNode withoutSignature = NodeFinder.perform(
+					node.getAlternateRoot(), start, 0);
+			System.out.println(withoutSignature.getParent());
+
+			System.out.println("@@@@@");
+			System.out.println("END LINE COMMENT");
+
+			return true;
+
+		}
+
+		@Override
+		public boolean visit(BlockComment node) {
+			System.out.println("BEGIN BLOCK COMMENT");
+
+			int startLineNumber = compUnit.getLineNumber(node
+					.getStartPosition()) - 1;
+			int endLineNumber = compUnit.getLineNumber(node.getStartPosition()
+					+ node.getLength()) - 1;
+
+			StringBuffer blockComment = new StringBuffer();
+
+			for (int lineCount = startLineNumber; lineCount <= endLineNumber; lineCount++) {
+
+				String blockCommentLine = source[lineCount].trim();
+				blockComment.append(blockCommentLine);
+				if (lineCount != endLineNumber) {
+					blockComment.append("\n");
+				}
+			}
+
+			System.out.println("@@@@@");
+			System.out.println(blockComment.toString());
+
+			int start = node.getStartPosition();
+			ASTNode withoutSignature = NodeFinder.perform(
+					node.getAlternateRoot(), start, 0);
+			System.out.println(withoutSignature.getParent());
+
+			System.out.println("@@@@@");
+
+			System.out.println("END BLOCK COMMENT");
+
 			return true;
 		}
-		
-		
 
 	}
-	
+
 }
