80ac05a738accd84ae50c7a33136cae8a66cf27f
Daniel Lyons <n3042091@tees.ac.uk>
2014-04-25 15:27:15 +0100

    Fixed issues with comments in XML Parser

--- XML Parser/src/xmlparser/XMLAnalysis.java
+++ XML Parser/src/xmlparser/XMLAnalysis.java
@@ -1,5 +1,7 @@
 package xmlparser;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -7,7 +9,6 @@ import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.ArrayCreation;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.BlockComment;
 import org.eclipse.jdt.core.dom.Comment;
@@ -17,12 +18,13 @@ import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.LineComment;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.NodeFinder;
-import org.eclipse.jdt.core.dom.SimplePropertyDescriptor;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TagElement;
-import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
-import org.jdom2.*;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
@@ -31,20 +33,23 @@ import edu.cmu.cs.crystal.AbstractCrystalMethodAnalysis;
 
 public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 
-	
 	Stack<Element> elements;
 	MethodDeclaration methodDec;
 	Document d;
 	long startTime;
 	long endTime;
 	XMLParser parser; // This is the comment visitor used to visit all
-								// the AST Nodes; it is initialised in
-								// beforeAllMethods()
+						// the AST Nodes; it is initialised in
+						// beforeAllMethods()
+
+	List<XMLMethod> methodList;
+	Element root;
 
 	// Called before all methods
 	@Override
-	public void beforeAllMethods(ITypeRoot rootNode, CompilationUnit compilationUnit) {
-		List<ASTNode> commentList1 = compilationUnit.getCommentList();
+	public void beforeAllMethods(ITypeRoot rootNode,
+			CompilationUnit compilationUnit) {
+		// List<ASTNode> commentList1 = compilationUnit.getCommentList();
 		elements = new Stack<Element>();
 		methodDec = null;
 		String source = null;
@@ -65,21 +70,36 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 		// initialize the cvisitor with the source
 		parser = new XMLParser(compilationUnit, source.split("\n"));
 
+		d = new Document();
+		root = new Element("class");
+		elements.add(root);
+		d.setRootElement(root);
+		compilationUnit.accept(parser);
+
+		// getList of methods
+		List<Element> el = root.getChildren();
+		methodList = new ArrayList<XMLMethod>();
+		for (Element e : el) {
+			if (e instanceof XMLMethod) {
+				methodList.add((XMLMethod) e);
+			}
+
+		}
+
 		// To visite LineComment and BlockComment nodes, we need to call the
 		// accept method on them
 		// (see documentation of getCommentList() for more details)
 		List<Comment> l = compilationUnit.getCommentList();
 		for (Comment c : l) {
-			if (!(c instanceof Javadoc)) {
+			if (c instanceof Javadoc && c.getParent() == null) {
 				c.accept(parser);
 			}
+			if (!(c instanceof Javadoc)) {
+				c.accept(parser);
+
+			}
 		}
-		
-		d = new Document();
-		Element root = new Element("class");
-		elements.add(root);
-		d.setRootElement(root);
-		compilationUnit.accept(parser);
+
 		// records start time of analysis
 		startTime = System.currentTimeMillis();
 		System.out.println("Running: " + this.getName());
@@ -92,40 +112,57 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 
 		// records end time
 		endTime = System.currentTimeMillis();
+		
+		XMLOutputter xml = new XMLOutputter();
+		xml.setFormat(Format.getPrettyFormat());
+		System.out.println(xml.outputString(d));
+		
 		// prints time taken
+		
 		System.out.println("\nEnd of " + getName() + ", \nTime Taken: "
 				+ (endTime - startTime) + "ms");
 		// create and run a test analysis
 		TestAnalysis analysis = new TestAnalysis();
 		analysis.runAnalysis(getReporter(), getInput(), compUnit, rootNode);
-		
-		XMLOutputter xml = new XMLOutputter();
-		xml.setFormat(Format.getPrettyFormat());
-		System.out.println(xml.outputString(d));
+
+
 		super.afterAllMethods(compUnit, rootNode);
 	}
 
 	@Override
 	public void analyzeMethod(MethodDeclaration d) {
-		//methodDec = d;
-		//d.accept(parser);
+		// methodDec = d;
+		// d.accept(parser);
 	}
 
 	@Override
 	public String getName() {
-		return "Comment Visitor";
+		return "XML Parser";
 	}
 
 	/**
-	 * Issue with Parsing line and block comments as the parser runs for  
-	 * comments and then the rest of the class as accept() needs to be 
-	 * called on each comment
+	 * Comments other than method or field javadocs are added at the bottom of 
+	 * their containing method or class
 	 */
 	private class XMLParser extends ASTVisitor {
 
 		CompilationUnit compUnit;
 		String[] source;
-	
+		int methodCount = 0;
+		boolean classNameSet = false;
+		
+		
+
+
+		@Override
+		public void endVisit(SimpleName node) {
+			if(!classNameSet){
+				root.setAttribute("name", node.toString());
+				classNameSet = true;
+			}
+			
+			super.endVisit(node);
+		}
 
 		public XMLParser(CompilationUnit c, String[] s) {
 			compUnit = c;
@@ -135,31 +172,41 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 		@Override
 		public boolean visit(Block node) {
 			Element e;
-			if(methodDec != null){
-				//if block is a method add appropriate attributes
-				e = new Element("method");	
-				Attribute type = new Attribute("type",methodDec.getReturnType2().toString());
-				Attribute name = new Attribute("name", methodDec.getName().toString());
-				e.setAttribute(type);
-				e.setAttribute(name);
+			if (methodDec != null) {
+				methodCount++;
+				e = new XMLMethod(methodCount);
+				Attribute type = new Attribute("type", methodDec
+						.getReturnType2().toString());
+				Attribute name = new Attribute("name", methodDec.getName()
+						.toString());
+				int startLine = compUnit.getLineNumber(node.getStartPosition());
+
+				((XMLMethod) e).setType(type);
+				((XMLMethod) e).setMethodName(name);
+				((XMLMethod) e).setStartLine(startLine);
+				((XMLMethod) e).setEndLine(compUnit.getLineNumber(node.getStartPosition() + node.getLength()));
 				methodDec = null;
-			}else{
-				 e = new Element("scope");
+			} else {
+				e = new Element("scope");
 			}
-			//adds block to previous top of stack
+			// adds block to previous top of stack
 			elements.peek().addContent(e);
-			//pushes block to top of stack
+			// pushes block to top of stack
 			elements.push(e);
 			return super.visit(node);
 		}
+
+		
+		
+		
 		
 		@Override
 		public void endVisit(Block node) {
-			//removes block from stack
+			// removes block from stack
 			elements.pop();
 			super.endVisit(node);
 		}
-	
+
 		@Override
 		public boolean visit(MethodDeclaration node) {
 			methodDec = node;
@@ -168,74 +215,109 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 
 		@Override
 		public void endVisit(FieldDeclaration node) {
-	
+
 			Element e = new Element("declaration");
-			Attribute type = new Attribute("type",node.getType().toString());
-			String nameS = ((VariableDeclarationFragment)node.fragments().get(0)).getName().toString();
-			Attribute name = new Attribute("name",nameS);
+			Attribute type = new Attribute("type", node.getType().toString());
+			String nameS = ((VariableDeclarationFragment) node.fragments().get(
+					0)).getName().toString();
+			Attribute name = new Attribute("name", nameS);
 			e.setAttribute(type);
 			e.setAttribute(name);
 			elements.peek().addContent(e);
 			super.endVisit(node);
 		}
-	
+
 		@Override
 		public boolean visit(VariableDeclarationStatement node) {
-			//getType() ignores arrays so check for array added
+			// getType() ignores arrays so check for array added
 			String typeS = node.getType().toString();
-			if(node.fragments().get(0).toString().contains("[]")){
+			if (node.fragments().get(0).toString().contains("[]")) {
 				typeS = typeS + "[]";
 			}
 			Element declaration = new Element("declaration");
-			Attribute type = new Attribute("type" ,typeS);
+			Attribute type = new Attribute("type", typeS);
 			declaration.setAttribute(type);
-			
-			String nameS = ((VariableDeclarationFragment)node.fragments().get(0)).getName().toString();
-			Attribute name = new Attribute("name" ,nameS);
+
+			String nameS = ((VariableDeclarationFragment) node.fragments().get(
+					0)).getName().toString();
+			Attribute name = new Attribute("name", nameS);
 			declaration.setAttribute(name);
 			elements.peek().addContent(declaration);
 			super.endVisit(node);
 			return super.visit(node);
 		}
 
-		
-
 		@Override
 		public boolean visit(Javadoc node) {
-			Element javadoc  = new Element("javadoc");
-			List<TagElement> tags = node.tags() ;
-			for(TagElement t : tags){
-				
-				if(t.getTagName() == null){
+			Element javadoc = new Element("javadoc");
+			List<TagElement> tags = node.tags();
+			for (TagElement t : tags) {
+
+				if (t.getTagName() == null) {
 					Element tag = new Element("text");
 					tag.addContent(t.toString().substring(3));
 					javadoc.addContent(tag);
-				}else{
+				} else {
 					Element tag = new Element(t.getTagName().substring(1));
-					tag.addContent(t.toString().substring(t.getTagName().toString().length()+4));
-					
+					tag.addContent(t.toString().substring(
+							t.getTagName().toString().length() + 4));
+
 					javadoc.addContent(tag);
 				}
 			}
+			if (node.getParent() == null) {
+				boolean inMethod = false;
+				int startLineNumber = compUnit.getLineNumber(node
+						.getStartPosition());
 				
-			elements.peek().addContent(javadoc);
+				for (XMLMethod e : methodList) {
+					
+					if (startLineNumber >= e.getStartLine()
+							&& startLineNumber <= e.getEndLine()) {
+						e.addContent(javadoc);
+						inMethod = true;
+						break;
+					}
+				}
+				if (!inMethod) {
+					root.addContent(javadoc);
+				}
+			} else {
+				elements.peek().addContent(javadoc);
+			}
 			return super.visit(node);
 		}
 
-
 		@Override
-		public boolean visit(LineComment node) {			
+		public boolean visit(LineComment node) {
 			int startLineNumber = compUnit.getLineNumber(node
-					.getStartPosition()) - 1;
-			String lineComment = source[startLineNumber].trim();
+					.getStartPosition()) ;
+			String lineComment = source[startLineNumber-1].trim();
 			lineComment = lineComment.substring(lineComment.indexOf("//"));
+
 			
+			
+			
+			Element comment = new Element("comment");
+			comment.addContent(lineComment.toString().replaceAll("//", ""));
+			boolean inMethod =false;
+			for (XMLMethod e : methodList) {
+				
+				if (startLineNumber+1 >= e.getStartLine()
+						&& startLineNumber <= e.getEndLine()) {
+					e.addContent(comment);
+					inMethod = true;
+					break;
+				}
+			}
+			if(!inMethod){
+				root.addContent(comment);
+			}
 			int start = node.getStartPosition();
 			ASTNode withoutSignature = NodeFinder.perform(
 					node.getAlternateRoot(), start, 0);
-			Element comment = new Element("comment");
-			comment.addContent(lineComment.toString());
-			elements.peek().addContent(comment);
+			
+			
 
 			return true;
 
@@ -243,14 +325,15 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 
 		@Override
 		public boolean visit(BlockComment node) {
+
 			int startLineNumber = compUnit.getLineNumber(node
-					.getStartPosition()) - 1;
+					.getStartPosition()) ;
 			int endLineNumber = compUnit.getLineNumber(node.getStartPosition()
-					+ node.getLength()) - 1;
+					+ node.getLength()) ;
 
 			StringBuffer blockComment = new StringBuffer();
 
-			for (int lineCount = startLineNumber; lineCount <= endLineNumber; lineCount++) {
+			for (int lineCount = startLineNumber-1; lineCount <= endLineNumber-1; lineCount++) {
 
 				String blockCommentLine = source[lineCount].trim();
 				blockComment.append(blockCommentLine);
@@ -258,14 +341,27 @@ public class XMLAnalysis extends AbstractCrystalMethodAnalysis {
 					blockComment.append("\n");
 				}
 			}
-			Element comment =  new Element("comment");
-			comment.addContent(blockComment.toString());
-			elements.peek().addContent(comment);
+			Element comment = new Element("comment");
+			comment.addContent(blockComment.toString().replaceAll("\\*", "")
+					.replaceAll("/", ""));
 
+			boolean inMethod =false;
+			for (XMLMethod e : methodList) {
+			
+				if (startLineNumber+1 >= e.getStartLine()
+						&& startLineNumber <= e.getEndLine()) {
+					e.addContent(comment);
+					inMethod = true;
+					break;
+				}
+			}
+			if(!inMethod){
+				root.addContent(comment);
+			}
 			int start = node.getStartPosition();
 			ASTNode withoutSignature = NodeFinder.perform(
 					node.getAlternateRoot(), start, 0);
-			
+
 			return true;
 		}
 
