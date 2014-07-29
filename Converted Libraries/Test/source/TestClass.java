09d03385aa0e8da6aafbaaf7c6bf19292d25310c
Joao F. Ferreira <joao@joaoff.com>
2014-07-29 13:58:19 +0100

    Updated converted libraries (copied Daniel's local/eclipse/Converted\ Libraries folder).

---
+++ Converted Libraries/Test/source/TestClass.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+public class TestClass {
+  int i;
+  List<Integer> l=new ArrayList();
+  TestClass t;
+  public void a(){
+    int c;
+    t=new TestClass();
+  }
+  public void b(){
+    t.toString();
+    int i=0;
+  }
+}
